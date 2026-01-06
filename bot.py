6eb9f4aefd39af426dfba0b6467d996ed57595c8
cid1228 <samoyloviliya1228@gmail.com>
2026-01-06 17:43:42 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    WebAppInfo
+)
+
+TOKEN = ""
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+
+    @dp.message(CommandStart())
+    async def start(message: types.Message):
+        markup = ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(
+                        text="Открыть кпокак",
+                        web_app=WebAppInfo(url="https://cid1228.github.io/MyFirstRepository/")
+                    )
+                ]
+            ],
+            resize_keyboard=True
+        )
+
+        await message.answer(
+            "зомби блять выживание скачать без смс и регистрации",
+            reply_markup=markup
+        )
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
